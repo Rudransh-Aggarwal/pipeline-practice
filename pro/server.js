1a0ac9718d1bf29f957797c57d15a689f3c1636b
Rudransh Aggarwal <rudranshaggarwal1507@gmail.com>
2026-02-20 19:34:30 +0530

    test-34

--- pro/server.js
+++ pro/server.js
@@ -5,7 +5,7 @@ const app = express();
 app.use(express.json());
 
 // Connect MongoDB
-mongoose.connect("mongodb://mongo:27017/db", {})
+mongoose.connect("mongodb://mongodb:27017/students", {})
 .then(() => console.log("MongoDB Connected"))
 .catch(err => console.log(err));
 
